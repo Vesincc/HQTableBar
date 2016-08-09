2111096c9a84f084e4c89d53a0e383735166f6ec
HanQi <1464467970@qq.com>
2016-08-09 16:37:15 +0800

    tabBar

--- TableBar/ViewController.m
+++ TableBar/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "testViewController.h"
+#import "HQTabBar.h"
 
-@interface ViewController ()
+@interface ViewController () <HQTabBarDelegate, UITabBarControllerDelegate>
+
+@property (nonatomic, strong) UITabBarController *tab;
 
 @end
 
@@ -16,7 +20,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.tab = [[UITabBarController alloc] init];
+    
+    [UIApplication sharedApplication].delegate.window.rootViewController = self.tab;
+    
+    [self addSubviewController:self title:@"首页" image:@"tabbar_home" selectedImage:@"tabbar_home_selected"];
+    
+    [self addSubviewController:[[testViewController alloc] init] title:@"消息" image:@"tabbar_message_center" selectedImage:@"tabbar_message_center_selected"];
+    
+    
+    
+    
+    
+    HQTabBar *tabBar = [[HQTabBar alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, 50) tabBarSize:3];
+    tabBar.delegate = self;
+    [self.tab setValue:tabBar forKey:@"tabBar"];
+    self.tab.tabBar.frame = CGRectMake(0, [[UIScreen mainScreen] bounds].size.height - 55,[[UIScreen mainScreen] bounds].size.width, 55);
+    self.tab.tabBar.opaque=YES;
+    
+    
 }
+
+- (void)addSubviewController:(UIViewController *)viewController title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage {
+
+    viewController.title = title;
+    
+    viewController.tabBarItem.image = [[UIImage imageNamed:image] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ 
+    viewController.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+    [viewController.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName : [[UIColor alloc] initWithRed:123/255.0 green:123/255.0 blue:123/255.0 alpha:1]} forState:UIControlStateNormal];
+    [viewController.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor orangeColor]} forState:UIControlStateSelected];
+
+    [self.tab addChildViewController:viewController];
+}
+
+- (void)hQTabBar:(HQTabBar *)tabBar btnDidClick:(UIButton *)button {
+
+    NSLog(@"231");
+
+}
+
 
 @end
--- TableBar/testViewController.h
+++ TableBar/testViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  testViewController.h
 //  TableBar
 //
 //  Created by HanQi on 16/8/8.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController 
-
+@interface testViewController : UIViewController
 
 @end
-
